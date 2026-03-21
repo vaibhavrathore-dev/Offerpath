e6cd7f1a642f4abc9171550ff2ec62b9c319cce4
vaibhavrathore-dev <vaibhavrathore911@gmail.com>
2026-03-21 10:43:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -201,6 +201,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True     # auto-restart when you edit code
+        port=8080,
+        reload=False
     )
