1a4c09c008067e6803012448b43949762487f1e7
vaibhavrathore-dev <vaibhavrathore911@gmail.com>
2026-03-22 11:15:20 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -17,7 +17,6 @@ MODELS = [
     "meta-llama/llama-3.3-70b-instruct:free",
     "meta-llama/llama-3.2-3b-instruct:free",
     "mistralai/mistral-small-3.1-24b-instruct:free",
-    "nousresearch/hermes-3-405b-instruct:free",
     "qwen/qwen3-4b:free",
 ]
 def call_ai(prompt):
